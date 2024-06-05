1166ebd8bf354f190cf84c427a1a31634fc2d77c
Ben Dowen <dirtydevil@gmail.com>
2024-06-05 19:50:31 +0100

    get list of snacks from bff and display

--- snack-shop-fe/src/App.tsx
+++ snack-shop-fe/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDataForSnacks = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/snacks`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let snackData = await response.json();
+        setData(snackData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataForSnacks();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+        {loading && (
+          <div>Loading snacks...</div>
+        )}
+        {error && <div>{error}</div>}
+
+        <ul>
+          {data &&
+            data.map(({ _id, snackName, details}) => (
+              <li
+                key={_id}
+               >
+                  {snackName}: {details.flavour}, {details.weight}
+              </li>
+            ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
-}
+};
 
-export default App
+export default App;
