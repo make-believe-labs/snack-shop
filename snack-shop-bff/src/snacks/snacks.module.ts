e34698dfc33a60c036e65474efedb761e676b7d2
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2024-06-27 16:20:57 +0100

    Dockerise

--- snack-shop-bff/src/snacks/snacks.module.ts
+++ snack-shop-bff/src/snacks/snacks.module.ts
@@ -2,10 +2,11 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SnacksController } from './snacks.controller';
 import { SnacksService } from './snacks.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [SnacksController],
-  imports: [HttpModule],
+  imports: [ConfigModule, HttpModule],
   providers: [SnacksService],
   exports: [SnacksService]
 })
