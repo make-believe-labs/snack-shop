f31ab4b98c0efd546d5074b481f2ac4892a24ca5
lisihocke <lisihocke@users.noreply.github.com>
2024-07-03 23:03:45 +0200

    bff: add order service tests

--- snack-shop-bff/src/orders/orders.service.spec.ts
+++ snack-shop-bff/src/orders/orders.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { OrdersService } from './orders.service';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
 
 describe('OrdersService', () => {
-  let service: OrdersService;
+  let orderService: OrdersService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,10 +15,55 @@ describe('OrdersService', () => {
       providers: [OrdersService],
     }).compile();
 
-    service = module.get<OrdersService>(OrdersService);
+    orderService = module.get<OrdersService>(OrdersService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(orderService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    describe('if orders exist', () => {
+      it('should return all orders', () => {
+        const response: AxiosResponse = {
+          data: [{_id: 1, orderTotal: 100},{_id: 2, orderTotal: 1234}],
+          status: 200,
+          statusText: 'OK',
+          headers: {},
+          config: {
+            headers: undefined
+          }
+        };
+        const expectedResult = response.data;
+    
+        jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(response));
+    
+        return orderService.getOrders().then(result => {
+          expect(result).toBe(expectedResult);
+        });
+      });
+    });
+
+    describe('if no orders exist', () => {
+      it('should return an empty array', () => {
+        const response: AxiosResponse = {
+          data: [],
+          status: 200,
+          statusText: 'OK',
+          headers: {},
+          config: {
+            headers: undefined
+          }
+        };
+        const expectedResult = response.data;
+    
+        jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(response));
+    
+        return orderService.getOrders().then(result => {
+          expect(result).toBe(expectedResult);
+        });
+      });
+    });
   });
 });
