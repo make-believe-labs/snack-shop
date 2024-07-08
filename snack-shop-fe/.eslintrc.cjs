d807e90fe1e52acfc426dbd356fd1915f85e3a78
Ben Dowen <dirtydevil@gmail.com>
2024-07-08 22:01:59 +0100

    fix linting issues and update eslint

--- snack-shop-fe/.eslintrc.cjs
+++ snack-shop-fe/.eslintrc.cjs
@@ -12,9 +12,8 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-      { "react/jsx-uses-react": "off" },
-      { "react/react-in-jsx-scope": "off" },
     ],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 }
--- snack-shop-fe/src/Components/ProductList.tsx
+++ snack-shop-fe/src/Components/ProductList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Wrapper } from './Wrapper';
+// eslint-disable-next-line no-redeclare
 import { Box, Text } from '@chakra-ui/react';
 
 interface snackItems {
@@ -62,14 +63,14 @@ export const Products: React.FC = () => {
             <Wrapper>
                 {data &&
                     data.map(({ _id, snackName, details, unitPrice, stock, categories }: snackItems) => (
-                        <Box id={_id} bg='green.100' p={4}>
+                        <Box id={_id} bg='green.100' p={4} key={_id}>
                             <Text as="b">{snackName}</Text>
                             <Text>{details.flavour}</Text>
                             <Text>{details.weight}</Text>
                             <Text>£{unitPrice / 100}</Text>
                             <Text>{stock > 0 ? 'In stock' : 'Out of stock'}</Text>
-                            {categories && categories.map(({categories}: snackCategories) => (
-                                <Text as="i">{categories} </Text>
+                            {categories && categories.map(({categories}: snackCategories, index) => (
+                                <Text as="i" key={index}>{categories}</Text>
                             ))}
                         </Box>
                     ))}
