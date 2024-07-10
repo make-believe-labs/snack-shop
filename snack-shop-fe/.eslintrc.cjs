d9bb1991c4a74ba30f6d0d6d3c605735bff0dc80
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2024-07-10 12:27:10 +0100

    fix marks

--- snack-shop-fe/.eslintrc.cjs
+++ snack-shop-fe/.eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
     ],
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 }
