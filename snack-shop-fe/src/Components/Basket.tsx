64d9984daf8981db703a670079a07a033be91cc7
Ben Dowen <ben@dowen.me.uk>
2024-08-28 12:59:36 +0100

    make basket visuals more compact

--- snack-shop-fe/src/Components/Basket.tsx
+++ snack-shop-fe/src/Components/Basket.tsx
@@ -1,7 +1,7 @@
 import { WrapperSingle } from './Wrapper';
 // eslint-disable-next-line no-redeclare
-import { Box, Card, CardBody, CardHeader, Heading, Text} from '@chakra-ui/react';
-import { RootState} from '../store'
+import { Box, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react';
+import { RootState } from '../store'
 import { useSelector } from 'react-redux';
 
 function Basket() {
@@ -12,18 +12,17 @@ function Basket() {
         <>
             <Box as='section' id='basket'>
                 <WrapperSingle>
-                    {list.snacksInBasket?.map(({ _id, snackName, details, unitPrice, categories }, index: number) => (
-                        <Card as='article' minW='250px' id={_id+'_'+index} bg='yellow.100' p={4} key={_id+'_'+index} data-testid={'basketSnack_' + index}>
-                            <CardHeader bgColor={'yellow.300'}>
-                                <Heading as="h3" fontSize={'2em'}>{snackName}</Heading>
+                    {list.snacksInBasket?.map(({ _id, snackName, details, unitPrice }, index: number) => (
+                        <Card as='article' minW='250px' id={_id + '_' + index} bg='yellow.100' p={3} key={_id + '_' + index} data-testid={'basketSnack_' + index}>
+                            <CardHeader bgColor={'yellow.300'} p={2}>
+                                <Heading as="h3" fontSize={'1.6em'}>{snackName}</Heading>
                             </CardHeader>
-                            <CardBody bgColor='yellow.300'>
-                                <Text>Flavour: {details.flavour}</Text>
-                                <Text>Weight: {details.weight}</Text>
-                                {categories && categories.map((category, index) => (
-                                    <Text as="i" key={index}>Category: {category}</Text>
-                                ))}
-                                <Text>Price: £{unitPrice / 100}</Text>
+                            <CardBody bgColor='yellow.300' p={2}>
+                                <Text>
+                                    Flavour: {details.flavour},
+                                    Weight: {details.weight},
+                                    Price: £{unitPrice / 100}
+                                </Text>
                             </CardBody>
                         </Card>
                     ))}
