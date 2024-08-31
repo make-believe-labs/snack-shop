7c637c703fcfef467783bf3b1a84989cf0ab3778
Ben Dowen <ben@dowen.me.uk>
2024-08-31 13:31:23 +0100

    Add Snacks Test

---
+++ snack-shop-sit/tests/add-snacks.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+const snackShopUrl = 'http://localhost:9090/'
+
+test('test', async ({ page }) => {
+    await page.goto(`${snackShopUrl}`);
+    await page.getByTestId('storeSnack_0').getByRole('button', { name: 'Add' }).click();
+    await page.getByTestId('storeSnack_1').getByRole('button', { name: 'Add' }).click();
+    await page.getByTestId('storeSnack_2').getByRole('button', { name: 'Add' }).click();
+    await page.getByRole('link', { name: 'Basket (3)' }).click();
+    await expect(page.getByTestId('basketSnack_0').getByRole('heading', { name: 'Snacktastic Mixed Nuts' })).toBeVisible();
+});
