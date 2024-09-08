7c8a85782b82d6b20a8790de7e4c3daded0b0654
Ben Dowen <ben@dowen.me.uk>
2024-09-08 14:45:44 +0100

    add comment to api test

--- snack-shop-sit/api-tests/snacks.spec.ts
+++ snack-shop-sit/api-tests/snacks.spec.ts
@@ -3,6 +3,8 @@ import * as request from 'supertest'
 
 const url = 'http://localhost:3000'
 
+// const url = 'https://lab.fullsnacktester.com/api'
+
 describe('Snacks', () => {
     test('GET /snacks should return OK', () => {
         request(`${url}`)
