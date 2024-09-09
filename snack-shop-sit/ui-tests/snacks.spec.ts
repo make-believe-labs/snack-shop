37c780970740878973726e280d6ccc9b67b22ef3
Ben Dowen <ben@dowen.me.uk>
2024-09-09 20:54:31 +0100

    update example

--- snack-shop-sit/ui-tests/snacks.spec.ts
+++ snack-shop-sit/ui-tests/snacks.spec.ts
@@ -3,22 +3,22 @@ import { test, expect } from '@playwright/test';
 const snackShopUrl = 'https://lab.fullsnacktester.com'
 
 // Example solution to Challenge 1: Update a test
-// The reason the orginal test is broken, is because it's asserting based on old test data
+// The reason the original test is broken, is because it's asserting based on old test data
 // We either need to control the test data, or find a more generic way to do the assert.
 
-// Solution 1: More generalised assertion
+// Solution 1: More generalized assertion
 test('Snacks added on the homepage should be displayed in the basket page', async ({ page }) => {
     await page.goto(`${snackShopUrl}`);
     await page.getByTestId('storeSnack_0').getByRole('button', { name: 'Add' }).click();
 
     // Save the name of the first snack we add, so we can assert on it later.
-    // We could instead controll the inventory of snacks for the test.
+    // We could instead control the inventory of snacks for the test.
     const snackName = await page.getByTestId('storeSnack_0').getByRole('heading').innerText();
     
     await page.getByTestId('storeSnack_1').getByRole('button', { name: 'Add' }).click();
     await page.getByTestId('storeSnack_2').getByRole('button', { name: 'Add' }).click();
     
-    // Use regex to find te basket link, reguardless of how many items added.
+    // Use regex to find te basket link, regardless of how many items added.
     // This could be improved, by adding a testId to the navigation links.
     await page.getByText(/^Basket/).click();
 
